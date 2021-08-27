03d3ac6a195ff216f0a1da8dc42a6f947b42fa21
Vladimir Sukhikh <vsukhikh@icloud.com>
2021-08-27 10:12:43 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Владимир Сухих on 27.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
